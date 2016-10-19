6083ab051b41db9ac58d256b46727699ce87d56e
Philip Washington Sorst <philip@sorst.net>
2016-10-19 12:01:00 +0200

    Adding isSame

--- src/Dontdrinkandroot/Entity/GeneratedIntegerIdEntity.php
+++ src/Dontdrinkandroot/Entity/GeneratedIntegerIdEntity.php
@@ -2,6 +2,8 @@
 
 namespace Dontdrinkandroot\Entity;
 
+use Doctrine\Common\Util\ClassUtils;
+
 class GeneratedIntegerIdEntity implements IntegerIdEntityInterface
 {
     /** @var int */
@@ -25,5 +27,31 @@ class GeneratedIntegerIdEntity implements IntegerIdEntityInterface
     public function isPersisted()
     {
         return null !== $this->id;
+    }
+
+    /**
+     * Checks if this entity represents the same entity as another one. Usually this is checked via the class and id.
+     *
+     * @param mixed $other
+     *
+     * @return bool
+     */
+    public function isSame($other): bool
+    {
+        if (null === $other || !is_object($other)) {
+            return false;
+        }
+
+        $thisClass = ClassUtils::getRealClass(get_class($this));
+        $otherClass = ClassUtils::getRealClass(get_class($other));
+
+        if (!$thisClass === $otherClass) {
+            return false;
+        }
+
+        /** @var AbstractEntity $otherEntity */
+        $otherEntity = $other;
+
+        return $this->getId() === $otherEntity->getId();
     }
 }
