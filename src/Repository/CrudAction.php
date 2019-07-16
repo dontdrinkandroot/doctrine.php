3e1bb9cf324b9099de9d0617303cfb14922015e2
Philip Washington Sorst <philip@sorst.net>
2019-07-16 19:04:02 +0200

    Adding CrudAction

---
+++ src/Repository/CrudAction.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Dontdrinkandroot\Repository;
+
+/**
+ * @author Philip Washington Sorst <philip@sorst.net>
+ */
+class CrudAction
+{
+    const CREATE = 'CREATE';
+    const READ = 'READ';
+    const UPDATE = 'UPDATE';
+    const DELETE = 'DELETE';
+}
